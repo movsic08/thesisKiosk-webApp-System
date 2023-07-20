0e2acf90047094d5665c842d180920e0dadd418b
movsic08 <lejero08@gmail.com>
2023-07-20 14:18:38 +0800

    Tailwind CSS Installed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>SAMPLE LANG HASHAHSAH</title>
+        <title>SAMPLE LANG HASHAHSAHgit</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/sidebar', function(){
+    return view('sidebar');
+});
 
 Route::get('/sample', function(){
     return dd();
